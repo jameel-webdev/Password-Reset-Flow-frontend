3093ffb87a1a0eb2a7f2f3229c90fddc0db4ae97
Jameel Ahmed <jameel.webdev@gmail.com>
2023-11-22 21:49:08 +0530

    Ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: "https://password-reset-flow-mernapp.netlify.app",
+    port: 3000,
     proxy: {
       "/api": {
         target: "http://localhost:5000",
