68806a69ef64a0ac16534d69b63a5f24e491c0d8
Jameel Ahmed <jameel.webdev@gmail.com>
2023-11-22 22:37:31 +0530

    /

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://password-reset-flow-9qw8.onrender.com",
+        target: "https://password-reset-flow-9qw8.onrender.com/",
         changeOrigin: true,
       },
     },
