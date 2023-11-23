04f456fc1db0686bb8d721ebe56fad1ec44b8dba
Jameel Ahmed <jameel.webdev@gmail.com>
2023-11-23 10:39:20 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        // whenever we go to fetch data from server side which starts "/api"
-        target: "https://password-reset-flow-is8m.onrender.com",
-        changeOrigin: true,
-      },
-    },
-  },
 });
